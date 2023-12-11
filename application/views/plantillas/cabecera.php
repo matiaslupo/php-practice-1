7aca3d204125dc9be68f81cd33086211e585a5f5
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-11 12:18:59 -0300

    Headers view file now shows view title

--- application/views/plantillas/cabecera.php
+++ application/views/plantillas/cabecera.php
@@ -8,6 +8,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title><?php if (isset($titulo)) echo $titulo; else echo "App"; ?></title>
   </head>
   <body>
