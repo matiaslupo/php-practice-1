dad45a967ebbb9cbe4361b009f5b4e42b5177a0c
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-11 19:37:38 -0300

    Login layou

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,1 +1,22 @@
-login
+<div class="container">
+    <div class="row py-5">
+        <div class="col-4 offset-4">
+            <div class="card py-2">
+                <h5 class="card-title text-center">Login</h5>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nombre de usuario</label>
+                            <input type="text" class="form-control" name="nick">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Clave</label>
+                            <input type="password" class="form-control" name="clave">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
