012cb46b8cf9341647b955cc34848fae2359e8ae
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-12 18:48:45 -0300

    Added login method

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -5,6 +5,18 @@ class usuarios_model extends CI_Model {
     private $tabla= 'usuarios';
     private $pk= 'usuario_id';
 
+    public function login($credenciales= array('nick' => '', 'clave' => '')){
+        $this->db->select($this->pk.", nick");
+        $this->db->where("nick", $credenciales['nick']);
+        $this->db->where("password", $credenciales['clave']);
+        $this->db->limit(1);
+        $res= $this->db->get($this->tabla);
+        if ($res->num_rows()){
+            return $res->row_array();
+        }
+        return false;
+    }
+
 }
 
 ?>
