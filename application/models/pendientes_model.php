7d7626d390db14d31e6751fc949b4efa0b8bb97d
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-12 19:35:10 -0300

    Todos listing feature

--- application/models/pendientes_model.php
+++ application/models/pendientes_model.php
@@ -5,6 +5,12 @@ class pendientes_model extends CI_Model {
     private $tabla= 'pendientes';
     private $pk= 'id_tarea';
 
+    public function listar_tareas($id= 0){
+        $this->db->where('usuario_id', $id);
+        $res= $this->db->get($this->tabla);
+        return $res->result_array();
+    }
+
 }
 
 ?>
