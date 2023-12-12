2472817aa223ca6c6ca86cdf856d0e4f6546d745
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-12 19:11:40 -0300

    Better navbar style

--- application/views/plantillas/navegacion.php
+++ application/views/plantillas/navegacion.php
@@ -1,22 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <a class="navbar-brand" href="#">TodoAPP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Seccion 1</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Seccion 2</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link ">Disabled</a>
+        <a class="nav-link" href="#">Seccion 3</a>
       </li>
     </ul>
+    <div class="my-2 my-lg-0">
+      <a class="btn btn-outline-danger my-2 my-sm-0" ><i class="bi bi-box-arrow-left"></i></a>
+    </div>    
   </div>
 </nav>
