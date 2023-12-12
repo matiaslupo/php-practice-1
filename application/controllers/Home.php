d2bcad508308c9295ad0074dbce9177ab12a7c10
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-12 19:35:30 -0300

    Session validation in Home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,12 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	private $f = "home/"; //view folder
+
     private $datos= array();
 	private $mostrar_barra= true;
+	private $id;
 
 	public function __construct(){
 		parent::__construct();
+		$this->validar_sesion();
+		$this->id= $_SESSION['usuario_id'];
 		$this->datos['titulo']= 'Practica PHP: Lista de tareas';
+		$this->load->model('pendientes');
 	}
 
 	public function index()
@@ -17,9 +22,16 @@ class Home extends CI_Controller {
 		redirect('home/main');
 	}
 
-    public function main(){
+    public function main(){		
         $this->mostrar();
     }
+
+	private function validar_sesion(){
+		if (isset($_SESSION['usuario_id']) == false){
+			redirect('login');
+			exit();
+		}
+	}
 
 	private function mostrar($vista= "home"){
         $this->load->view("plantillas/cabecera", $this->datos);
