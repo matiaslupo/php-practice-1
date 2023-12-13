f2f930d60842ff288aa311237c849d7ec7cbdcf5
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-13 20:30:39 -0300

    Added tabs to change view between pending and completed tasks

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -2,17 +2,18 @@
     <div class="row py-5">
         <div class="col-8 offset-2">
             <div class="card">
-                <div class="card-body">                    
+                <div class="card-body">
                     <h5 class="card-title">Nueva tarea</h5>
                     <form class="form-inline" method="post" action="<?php echo site_url('home/crear_tarea') ?>">
                         <label class="sr-only" for="inlineFormInputName2">Tarea</label>
                         <input type="text" class="form-control mb-2 mr-sm-2" name="texto">
-                        <button type="submit" class="btn btn-primary mb-2"><i class="bi bi-plus-square-fill"></i></button>
+                        <button type="submit" class="btn btn-primary mb-2"><i
+                                class="bi bi-plus-square-fill"></i></button>
                     </form>
-                    <?php if (isset($error)) { ?>
-                        <div class="alert alert-danger">
-                            <?php echo $error; ?>
-                        </div>
+                    <?php if (isset($_SESSION['error'])) { ?>
+                    <div class="alert alert-danger">
+                        <?php echo $_SESSION['error']; ?>
+                    </div>
                     <?php } ?>
                 </div>
             </div>
@@ -21,12 +22,20 @@
     <div class="row">
         <div class="col-8 offset-2">
             <div class="card">
-                <div class="card-body">                    
-                    <h5 class="card-title py-1 text-center">Lista de tareas de <?php echo $nick; ?></h5>
+                <div class="card-body">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active">Pendientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo site_url('home/completadas'); ?>">Completadas</a>
+                        </li>
+                    </ul>
+                    <h5 class="card-title py-3 text-center">Lista de tareas pendientes de <?php echo $nick; ?></h5>
                     <?php if (isset($info)) { ?>
-                        <div class="alert alert-info">
-                            No hay tareas pendientes.
-                        </div>
+                    <div class="alert alert-info">
+                        No hay tareas pendientes.
+                    </div>
                     <?php } ?>
                     <ul class="list-group">
                         <?php if (isset($pendientes)) { 
@@ -36,7 +45,8 @@
                             <span class="mr-2"><?php echo $p['texto']; ?></span>
                             <span class="mr-2"><?php echo $p['fecha']; ?></span>
                             <span class="float-right">
-                                <a href="#" class="btn btn-dark btn-sm mr-2"><i class="bi bi-eye-slash"></i></a>
+                                <a href="<?php echo site_url('home/completada/?id='.$p['id_tarea']); ?>"
+                                    class="btn btn-success btn-sm mr-2"><i class="bi bi-check-lg"></i></a>
                             </span>
                         </li>
                         <?php   }
