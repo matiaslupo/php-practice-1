aed90c3c7d4103811d46470939537082b0a162f3
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-13 20:30:13 -0300

    Added methods to complete, un-compelte and delete todos

--- application/models/pendientes_model.php
+++ application/models/pendientes_model.php
@@ -5,9 +5,9 @@ class pendientes_model extends CI_Model {
     private $tabla= 'pendientes';
     private $pk= 'id_tarea';
 
-    public function listar_tareas($id= 0){
+    public function listar_tareas($id= 0, $estado= TAREA_PENDIENTE){
         $this->db->where('usuario_id', $id);
-        $this->db->where('estado', TAREA_PENDIENTE);
+        $this->db->where('estado', $estado);
         $res= $this->db->get($this->tabla);
         return $res->result_array();
     }
@@ -19,6 +19,28 @@ class pendientes_model extends CI_Model {
         return false;
     }
 
+    public function completar($id= 0){
+        $this->db->set('estado', TAREA_COMPLETADA);
+        $this->db->where($this->pk, $id);
+        $this->db->limit(1);
+        $this->db->update($this->tabla);
+        return $this->db->affected_rows();
+    }
+
+    public function pendiente($id= 0){
+        $this->db->set('estado', TAREA_PENDIENTE);
+        $this->db->where($this->pk, $id);
+        $this->db->limit(1);
+        $this->db->update($this->tabla);
+        return $this->db->affected_rows();
+    }
+
+    public function borrar($id=0){
+        $this->db->where($this->pk, $id);
+        $this->db->limit(1);
+        $this->db->delete($this->tabla);
+        return $this->db->affected_rows();
+    }
 }
 
 ?>
